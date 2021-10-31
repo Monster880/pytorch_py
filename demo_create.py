cb9ff9bacbda4fc35bc55ad1282b5c00438dcd03
liding <539561425@qq.com>
2021-10-31 16:28:04 +0800

    fix: 修改代码

--- demo_create.py
+++ demo_create.py
@@ -20,3 +20,38 @@ b = torch.Tensor(2,3)
 b = torch.zeros_like(b)
 print(b)
 print(b.type())
+
+# 随机
+a = torch.rand(2,2)
+print(a)
+print(a.type())
+
+a = torch.normal(mean=0.0, std=torch.rand(5))
+print(a)
+print(a.type())
+
+a = torch.normal(mean=torch.rand(5), std=torch.rand(5))
+print(a)
+print(a.type())
+
+torch.Tensor(2,2).uniform_(-1, 1)
+print(a)
+print(a.type())
+
+# 序列
+a = torch.arange(0, 10, 1)
+print(a)
+print(a.type())
+
+a = torch.linspace(2, 10, 4)    # 拿到等间隔的n个数字
+print(a)
+print(a.type())
+
+a = torch.randperm(10)
+print(a)
+print(a.type())
+
+####
+import numpy as np
+a = np.array([[1,2],[2,3]])
+print(a)
