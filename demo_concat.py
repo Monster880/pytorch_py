07bcbfe171c8819def53c8235e37d97585a88758
liding <539561425@qq.com>
2021-11-06 17:39:45 +0800

    feat: 组合拼接

---
+++ demo_concat.py
@@ -0,0 +1,16 @@
+import torch
+
+a = torch.zeros((2,4))
+b = torch.ones((2,4))
+out = torch.cat((a,b), dim=1)
+print(out)
+
+#torch.stack
+print("torch.stack")
+a = torch.linspace(1,6,6).view(2,3)
+b = torch.linspace(7,12,6).view(2,3)
+print(a,b)
+out = torch.stack((a,b), dim=1)
+print(out,out.shape)
+print(out[:,:,0])
+print(out[:,:,1])
