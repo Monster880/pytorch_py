ee325753848dfd6597b98a747fae5008d90880cb
liding <539561425@qq.com>
2021-12-23 13:29:06 +0800

    feat: cifar10数据解析

--- cifar10.py
+++ cifar10.py
@@ -23,11 +23,35 @@ label_name = [
 ]
 
 import glob
+import numpy as np
+import os
 
-train_list = glob.glob("/Users/liding/Documents/pytorch_py/train/data_batch_*")
+train_list = glob.glob("/Users/liding/Documents/pytorch_py/test/test_batch")
 print(train_list)
+save_path = "/Users/liding/Documents/pytorch_py/test"
 
 for l in train_list:
     print(l)
     l_dict = unpickle(l)
-    print(l_dict)
+    # print(l_dict)
+    print(l_dict.keys())
+
+    for im_idx, im_data in enumerate(l_dict[b'data']):
+        # print(im_idx)
+        # print(im_data)
+
+        im_label = l_dict[b'labels'][im_idx]
+        im_name = l_dict[b'filenames'][im_idx]
+        print(im_label, im_name, im_data)
+
+        im_label_name = label_name[im_label]
+        im_data = np.reshape(im_data, [3, 32, 32])
+        im_data = np.transpose(im_data, (1, 2, 0))
+
+        # cv2.imshow("im_data", cv2.resize(im_data, (200, 200)))
+        # cv2.waitKey(0)
+
+        if not os.path.exists("{}/{}".format(save_path, im_label_name)):
+            os.mkdir("{}/{}".format(save_path, im_label_name))
+
+        cv2.imwrite("{}/{}/{}".format(save_path, im_label_name, im_name.decode("utf-8")), im_data)
